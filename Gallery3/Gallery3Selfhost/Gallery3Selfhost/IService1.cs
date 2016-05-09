25b7e4fea8e1db561b1f35352c1979a5194e3eda
Chris D'Entremont <Chris-DEntremont@live.nmit.ac.nz>
2016-05-09 14:21:34 +1200

    Lab.3.Gallery. Potentially namespace or reference errors? Parts iii / vii / viii / ix / x ???

---
+++ Gallery3/Gallery3Selfhost/Gallery3Selfhost/IService1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery3Selfhost
+{
+    [ServiceContract]
+    interface IService1
+    {
+        //Lab3.prt2.iv - "paste service routine header"
+        [OperationContract]
+        List<string> GetArtistNames();
+
+    }
+}
